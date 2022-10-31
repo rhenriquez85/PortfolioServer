e08e7784c3cbcb5d9c44be85947987f36f8a7213
EC2 Default User <ec2-user@ip-172-31-29-33.r3seprojects.com>
2022-10-31 21:02:53 +0000

    [card-info] add modal for viewing project description and details

--- public/scripts/card-modal.js
+++ public/scripts/card-modal.js
@@ -9,18 +9,35 @@
     
         const data = JSON.parse(xhr.responseText);
         data.cardData.forEach((card, index, arr) => {
+            if (!card.display) return;
             const newNode = document.createElement('div');
             newNode.innerHTML = template;
             const link = newNode.querySelector('.card-link');
             const modal = newNode.querySelector('.card-modal');
             const image = newNode.querySelector('.card-image');
             const title = newNode.querySelector('.card-title');
-    
+            const info = newNode.querySelector('.card-info');
+            const infoDesc = info.querySelector('.card-info-description');
+            const infoDetails = info.querySelector('.card-info-details');
+                
             link.href = card.url;
             modal.src = card.modal ? card.modal : data.defaultModal;
             image.src = card.image;
-            title.innerHTML = card.title;
-    
+            title.textContent = card.title;
+
+	        const cardDesc = card.description;
+	        const cardDetails = card.details;
+	        const regEx = /^[^,.;-]*:\s/;
+	        const titleDesc = cardDesc.match(regEx);
+	        const titleDetails = cardDetails.match(regEx);
+	        const titleHtml = '<strong class="card-info-title">@replace@</strong>';
+	        infoDesc.innerHTML = titleDesc !== -1 ? 
+	    	    cardDesc.replace(titleDesc[0], titleHtml.replace('@replace@', titleDesc[0])) : 
+	    	    cardDesc;
+	        infoDetails.innerHTML = titleDetails !== -1 ? 
+	    	    cardDetails.replace(titleDetails[0], titleHtml.replace('@replace@', titleDetails[0])) : 
+	    	    cardDetails;
+
             portfolioContainer.prepend(newNode);
 
             modal.addEventListener('mouseenter', () => {
@@ -28,6 +45,17 @@
             });
             modal.addEventListener('mouseleave', () => {
                 image.classList.remove('card-blur');
+            });
+            title.addEventListener('mouseenter', () => {
+                info.style.opacity = 1;
+                info.style.transitionProperty = 'opacity';
+                info.style.transitionDuration = '.5s';
+                info.style.zIndex = 2;
+            });
+            title.addEventListener('mouseleave', () => {
+                info.style.opacity = 0;
+                info.style.transitionDuration = '0s';
+                info.style.zIndex = -1;
             });
         });
     });
